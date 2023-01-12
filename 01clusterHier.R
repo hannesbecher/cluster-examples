894a7a3bc3b2e92eb8f81cecca0d2e3019eadcaf
Hannes Becher <hannesbecher@gmx.de>
2023-01-12 10:57:37 +0000

    plots

--- 01clusterHier.R
+++ 01clusterHier.R
@@ -4,7 +4,7 @@ setwd("~/git_repos/cluster-examples/")
 
 # Cusion dataset ---------------------------------------------------------
 
-dat01 <- read.table("data/01cussion.csv", 
+dat01 <- read.table("data/01cusion.csv", 
                     header=T,
                     sep=",")
 head(dat01)
@@ -48,3 +48,56 @@ plot(y~x,
 plot(y~x,
      col=cutree(cl01single, k=2),
      data=dat01)
+
+
+
+
+
+# Swirl data --------------------------------------------------------------
+
+
+
+dat02 <- read.table("data/02swirl.csv", 
+                    header=T,
+                    sep=",")
+head(dat02)
+plot(y~x,
+     col=as.numeric(as.factor(group)),
+     data=dat02)
+
+cl02 <- hclust(dist(dat02[,1:2]), method = "complete")
+
+cl02wd <- hclust(dist(dat02[,1:2]), method = "ward.D")
+cl02wd2 <- hclust(dist(dat02[,1:2]), method = "ward.D2")
+cl02single <- hclust(dist(dat02[,1:2]), method = "single")
+cl02average <- hclust(dist(dat02[,1:2]), method = "average")
+cl02mcquitty <- hclust(dist(dat02[,1:2]), method = "mcquitty")
+cl02median <- hclust(dist(dat02[,1:2]), method = "median")
+cl02centroid <- hclust(dist(dat02[,1:2]), method = "centroid")
+
+
+
+plot(y~x,
+     col=cutree(cl02, k=2),
+     data=dat02)
+plot(y~x,
+     col=cutree(cl02wd, k=2),
+     data=dat02)
+plot(y~x,
+     col=cutree(cl02wd2, k=2),
+     data=dat02)
+plot(y~x,
+     col=cutree(cl02average, k=2),
+     data=dat02)
+plot(y~x,
+     col=cutree(cl02centroid, k=2),
+     data=dat02)
+plot(y~x,
+     col=cutree(cl02median, k=2),
+     data=dat02)
+plot(y~x,
+     col=cutree(cl02mcquitty, k=2),
+     data=dat02)
+plot(y~x,
+     col=cutree(cl02single, k=2),
+     data=dat02)
---
+++ img/01makeImg.R
@@ -0,0 +1,37 @@
+# Make plots for README
+
+setwd("~/git_repos/cluster-examples/")
+
+# cusion
+dat01 <- read.table("data/01cusion.csv",
+                    header=T,
+                    sep=",")
+#png("img/cusion.png", width=6, height=4.5, units = "in", res=150)
+plot(y~x,
+     col=as.numeric(as.factor(group)),
+     data=dat01,
+     main="Cusion")
+#dev.off()
+
+# swirl
+dat02 <- read.table("data/02swirl.csv",
+                    header=T,
+                    sep=",")
+#png("img/swirl.png", width=5, height=5, units = "in", res=150)
+plot(y~x,
+     col=as.numeric(as.factor(group)),
+     data=dat02,
+     main="Swirl")
+#dev.off()
+
+
+# two blobs
+dat03 <- read.table("data/03twoBlobs.csv",
+                    header=T,
+                    sep=",")
+#png("img/twoBlobs.png", width=6, height=4.5, units = "in", res=150)
+plot(y~x,
+     col=as.numeric(as.factor(group)),
+     data=dat03,
+     main="Two blobs")
+#dev.off()
